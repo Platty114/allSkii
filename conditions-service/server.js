e04e95e7ae17f54d064383ef02d15699f13a22c9
Platty114 <jamesplatt02@gmail.com>
2024-03-17 04:03:16 -0600

    Add axios and setup call to open weather

--- conditions-service/server.js
+++ conditions-service/server.js
@@ -1,26 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import axios from "axios";
 
 //using dotenv parameters
 dotenv.config();
 
 
 
-//add cross access code here
-//
+//add cross access code here 
+//may be necessary, have had issues in the past with it.
 //
 
 
-const app = express();
-const port = process.env.PORT;
-const apiKey = process.env.API_KEY;
+const
+    app = express(),
+    port = process.env.PORT,
+    apiKey = process.env.API_KEY;
 
 
 app.use(express.json());
+    
 
-//add routes
+const
+    getWeatherInformation = async () => {
+        try {
+             
+        }
+        catch (err) {
+            console.log("Error retrieving weather data")
+        }
+    };
 
+
+//only need to return data based on latitude and longitude
+app
+  .get("/:lat/:long", (req, res) => {
+
+      //Grab openWeather data using axios
+       
+      
+
+  });
 
 
 //listen
