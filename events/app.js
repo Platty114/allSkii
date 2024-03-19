23f7e94c3536a8cd514a5acd8e91298617d50ec5
morganc20 <morgan.chen@ucalgary.ca>
2024-03-19 13:46:06 -0600

    post for events

--- events/app.js
+++ events/app.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const jsonWt = require('jsonwebtoken');
-const {getFirestore} = require('firebase-admin/firestore');
 
 const admin = require("firebase-admin");
 const credentials = require("./allskii-ecd4d-firebase-adminsdk-9lzcg-7efc3136c5.json");
@@ -13,17 +8,31 @@ admin.initializeApp({
     credential: admin.credential.cert(credentials)
 });
 
-app.post('/create', async (req, res) => {
-    try {
-
-    }
-});
-
-const db = admin.firestore();
-
 app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
+
+app.post('/create', async (req, res) => {
+    try {
+        console.log(req.body);
+        const id = req.body.id;
+        const eventJson = {
+            date: req.body.date,
+            hill: req.body.hill,
+            category: req.body.category,
+            name: req.body.name,
+            difficulty: req.body.difficulty,
+            pricing: req.body.pricing
+        };
+        const response = db.collection('Events').doc(id).set(eventJson);
+        res.send(response);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+const db = admin.firestore();
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
