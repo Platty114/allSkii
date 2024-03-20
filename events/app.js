2527908247a41fe5be58034851ff5a14750985ca
morganc20 <morgan.chen@ucalgary.ca>
2024-03-19 22:00:31 -0600

    Event ms inplemented

--- events/app.js
+++ events/app.js
@@ -12,10 +12,13 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
 
+const db = admin.firestore();
+
+// adding entries
 app.post('/create', async (req, res) => {
     try {
         console.log(req.body);
-        const id = req.body.id;
+        // const id = req.body.id;
         const eventJson = {
             date: req.body.date,
             hill: req.body.hill,
@@ -24,15 +27,65 @@ app.post('/create', async (req, res) => {
             difficulty: req.body.difficulty,
             pricing: req.body.pricing
         };
-        const response = db.collection('Events').doc(id).set(eventJson);
+        const response = await db.collection('Events').add(eventJson);
         res.send(response);
     } catch (error) {
+        console.log(error);
         res.status(500).send(error);
     }
-})
+});
 
-const db = admin.firestore();
+// get all entries
+app.get("/read/all", async (req, res) => {
+    try {
+        const eventsRef = db.collection('Events');
+        const response = await eventsRef.get();
+        let responseArray = [];
+        response.forEach(document => {
+            responseArray.push(document.data());
+        });
+        res.send(responseArray);
+    } catch (error) {
+        res.send(error);
+    }
+});
 
+// get one entry
+app.get("/read/:id", async (req, res) => {
+    try {
+        const eventsRef = db.collection('Events').doc(req.params.id);
+        const response = await eventsRef.get();
+        res.send(response.data());
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+// update entry
+// MAY NEED ID FOR EACH ENTRY TO MAKE THIS WORK < TO DO
+app.post('/update', async (req, res) => {
+    try {
+        const id = req.body.id;
+        const newHill = "Nakiska";
+        const eventsRef = await db.collection('Events').doc(id)
+        .update({
+            hill: newHill
+        });
+        res.send(eventsRef);
+    } catch(error) {
+        res.send(error);
+    }
+});
+
+// delete entry
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        const response = await db.collection('Events').doc(req.params.id).delete();
+        res.send(response);
+    } catch(error) {
+        res.send(error);
+    }
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
