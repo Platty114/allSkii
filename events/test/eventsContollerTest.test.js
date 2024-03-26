08a523b635870c9bf025b3cb8f403431f6bc58d7
morganc20 <morgan.chen@ucalgary.ca>
2024-03-26 17:39:25 -0600

    Testing done for events, moving on to reviews

--- events/test/eventsContollerTest.test.js
+++ events/test/eventsContollerTest.test.js
@@ -9,7 +9,8 @@ import {
 
 import {expect, jest, test} from '@jest/globals';
 
-describe("Testing GeoJsonAuthenticate", () => {
+// test for events authentication
+describe("Testing Events authentication", () => {
 
     //no session key
     test('no session key on request body should return status 401', async () => {
@@ -35,157 +36,110 @@ describe("Testing GeoJsonAuthenticate", () => {
 
 });
 
-
-describe("Testing GeoJson getAllGeoJson", () => {
-
-    //user status 200 should return all geojson data
-    test("User status 200 should return all geoJson data", () => {
-        const
-            req = {},
-            res = {
-                json: jest.fn(),
-                status: jest.fn()
-            },
-            user = {
-                status: 200
+// test for creating an event
+describe('Testing create an event', () => {
+    test('should add an event to the database and return a response', async () => {
+        const req = { 
+            body: { 
+                date: '2022-02-06 11:52:32', 
+                hill: 'Fernie', 
+                category: 'Snowboarding', 
+                name: 'Wipeout', 
+                difficulty: 'Hard', 
+                pricing: 20 } 
             };
-
-        res.status.mockImplementation((json) => {
-            return { json: res.json}
+        const res = {
+            send: jest.fn(),
+            status: jest.fn()
+        };
+        res.status.mockImplementation((send) => {
+            return { send: res.send}
         })
-
-        getAllGeoJson(user, req, res);
-
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledTimes(1);
-        expect(res.json).not.toHaveBeenCalledWith(null);
-    })
-
-    //user status 200 should return all geojson data
-    test("User status 500 should return error", () => {
-        const
-            req = {},
-            res = {
-                json: jest.fn(),
-                status: jest.fn()
-            },
-            user = {
-                status: 500
-            };
-
-        res.status.mockImplementation((json) => {
-            return { json: res.json}
-        })
-
-        getAllGeoJson(user, req, res);
-
-        expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({
-            error: "Internal service error"
-        });
-    })
-     
-    //user status 200 should return all geojson data
-    test("Any other user status should return 401", () => {
-        const
-            req = {},
-            res = {
-                json: jest.fn(),
-                status: jest.fn()
-            },
-            user = {
-                status: 300
-            };
-
-        res.status.mockImplementation((json) => {
-            return { json: res.json}
-        })
-
-        getAllGeoJson(user, req, res);
-
-        expect(res.status).toHaveBeenCalledWith(401);
-        expect(res.json).toHaveBeenCalledWith({
-            error: "unauthenticated user"
-        });
-    }) 
+        await createEvent(req, res);
+        expect(res.send).toHaveBeenCalled();
+        expect(res.send).toHaveBeenCalledTimes(1);
+        expect(res.send).not.toHaveBeenCalledWith(null);
+    });
 });
 
-describe("Testing GeoJson getGeoJsonBySkiHill", () => {
-
-    //user status 200 should return all geojson data
-    test("User status 200 should return all geoJson data for specified hill", () => {
-        const
-            req = { params: {
-                hillName: "Nakiska"
-            }},
-            res = {
-                json: jest.fn(),
-                status: jest.fn()
-            },
-            user = {
-                status: 200
-            };
-
-        res.status.mockImplementation((json) => {
-            return { json: res.json}
+// test for getting all events
+describe('Testing get all events', () => {
+    test('should return all events in the database', async () => {
+        const req = {};
+        const res = {
+            send: jest.fn(),
+            status: jest.fn()
+        };
+        res.status.mockImplementation((send) => {
+            return { send: res.send}
         })
+        await getAllEvents(req, res);
+        expect(res.send).toHaveBeenCalled();
+        expect(res.send).toHaveBeenCalledTimes(1);
+        expect(res.send).not.toHaveBeenCalledWith(null);
+        expect(Array.isArray(res.send.mock.calls[0][0])).toBe(true);
+    });
+});
 
-        getGeoJsonBySkiHill(user, req, res);
-
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledTimes(1);
-        expect(res.json).not.toHaveBeenCalledWith(null);
-    })
-
-    //user status 200 should return all geojson data
-    test("User status 500 should return error", () => {
-        const
-            req = { params: {
-                hillName: "Nakiska"
-            }},
-            res = {
-                json: jest.fn(),
-                status: jest.fn()
-            },
-            user = {
-                status: 500
-            };
-
-        res.status.mockImplementation((json) => {
-            return { json: res.json}
+// test for getting a single event
+describe('Testing get a single event', () => {
+    test('should return a single event from the database', async () => {
+        const req = { params: { id: 'eventID'} };
+        const res = {
+            send: jest.fn(),
+            status: jest.fn()
+        };
+        res.status.mockImplementation((send) => {
+            return { send: res.send}
         })
+        await getEvent(req, res);
+        expect(res.send).toHaveBeenCalled();
+        expect(res.send).toHaveBeenCalledTimes(1);
+        expect(res.send).not.toHaveBeenCalledWith(null);
+    });
+});
 
-        getGeoJsonBySkiHill(user, req, res);
-
-        expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({
-            error: "Internal service error"
-        });
-    })
-     
-    //user status 200 should return all geojson data
-    test("Any other user status should return 401", () => {
-        const
-            req = { params: {
-                hillName: "Nakiska"
-            }},
-            res = {
-                json: jest.fn(),
-                status: jest.fn()
-            },
-            user = {
-                status: 300
+// test for updating an event
+describe('Testing update an event', () => {
+    test('should update an event in the database and return a response', async () => {
+        const req = { 
+            body: { 
+                id: 's8dfbnidjfgi3d99df',
+                date: '2019-11-16 01:22:52', 
+                hill: 'COP', 
+                category: 'Skiing', 
+                name: 'Whiteout', 
+                difficulty: 'Easy', 
+                pricing: 75 } 
             };
-
-        res.status.mockImplementation((json) => {
-            return { json: res.json}
+        const res = {
+            send: jest.fn(),
+            status: jest.fn()
+        };
+        res.status.mockImplementation((send) => {
+            return { send: res.send}
         })
+        await updateEvent(req, res);
+        expect(res.send).toHaveBeenCalled();
+        expect(res.send).toHaveBeenCalledTimes(1);
+        expect(res.send).not.toHaveBeenCalledWith(null);
+    });
+});
 
-        getGeoJsonBySkiHill(user, req, res);
-
-        expect(res.status).toHaveBeenCalledWith(401);
-        expect(res.json).toHaveBeenCalledWith({
-            error: "unauthenticated user"
-        });
-    }) 
-})
+// test for deleting an event
+describe('Testing delete an event', () => {
+    test('should delete an event from the database and return a response', async () => {
+        const req = { params: { id: 'eventID'} };
+        const res = {
+            send: jest.fn(),
+            status: jest.fn()
+        };
+        res.status.mockImplementation((send) => {
+            return { send: res.send}
+        })
+        await deleteEvent(req, res);
+        expect(res.send).toHaveBeenCalled();
+        expect(res.send).toHaveBeenCalledTimes(1);
+        expect(res.send).not.toHaveBeenCalledWith(null);
+    });
+});
